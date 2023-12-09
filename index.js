a7b3c90bd54fe90b4a7aa140c32f99e700f14e43
KeyzarRasya <keyzar.r.athallah@gmail.com>
2023-12-09 17:42:13 +0700

    daslis commit

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
-const DasarListrik = require("./SemesterSatu/DasarListrik")
-const LinkedList = require('./SemesterDua/ASD/LinkedList')
+const DasarListrik = require("./Materi/DasarListrik")
+const LinkedList = require('./Materi/LinkedList')
 
 const ds = new DasarListrik();
 
 ds.findR("6V", "2A", (result) => {
+    console.log(result)
+})
+
+ds.findI("6v", "100Ohm", (result) => {
+    console.log(result)
+})
+
+ds.findV("2mA", "250Ohm", (result) => {
     console.log(result)
 })
